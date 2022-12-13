61673e80be63100a2c547b2c38eee43caeea447d
David Aditya Winarto <55628996+DavidAdityaW@users.noreply.github.com>
2022-12-13 12:09:03 +0700

    UT V3 David Aditya Winarto

---
+++ test ut v3.php
@@ -0,0 +1,24 @@
+<?php
+    function dataGedungLoker($no_loker) {
+        $lantai = 1; 
+        $loker = 1; 
+        $isFinish = false; 
+        
+        while (!$isFinish) { 
+            for ($i = 5; $i <= 7; $i++) { 
+                if ($no_loker >= $loker && $no_loker <= $loker + $i - 1) { 
+                    $isFinish = true;
+                    break; 
+                }
+                $lantai++;
+                $loker += $i;
+            }
+        }
+        
+        return $lantai;
+    }
+
+    // silahkan input parameter nomor lantai 
+    echo 'Lt. ' . dataGedungLoker(19);
+?>
+
